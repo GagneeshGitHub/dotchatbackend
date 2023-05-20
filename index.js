eb8ad6d215fed62b1ac5e5866fc18f0aac1fed47
GagneeshGitHub <gagneeshvimal@gmail.com>
2023-05-20 16:35:18 +0530

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = 8081;
+const port = process.env.PORT || 8081;
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
